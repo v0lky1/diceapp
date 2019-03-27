46595e9b4372d46226b1b3db0cbfe3f5096a8573
v0lky1 <460318@student.saxion.nl>
2019-03-27 20:58:03 +0100

    initial commit

---
+++ app/src/main/java/com/v0lky1/diceapp/Models/DiceModel.java
@@ -0,0 +1,17 @@
+package com.v0lky1.diceapp.Models;
+
+public class DiceModel {
+    private int numberDice;
+
+    public DiceModel() {
+    }
+
+    public int getNumberDice() {
+        return numberDice;
+    }
+
+    public void setNumberDice(int number) {
+        numberDice = number;
+    }
+
+}
